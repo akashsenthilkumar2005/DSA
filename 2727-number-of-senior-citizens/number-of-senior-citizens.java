890aa780a600462092aab0811e824b1ce96d1660
Akash S <akashsenthilkumar2005@gmail.com>
2025-12-14 20:56:24 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (58.85%) - LeetSync

---
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int c=0;
+        for(int i=0;i<details.length;i++){
+            int a=(details[i].charAt(11)-'0')*10+(details[i].charAt(12)-'0');
+            if(a>60) c++;
+        }
+        return c;
+    }
+}
