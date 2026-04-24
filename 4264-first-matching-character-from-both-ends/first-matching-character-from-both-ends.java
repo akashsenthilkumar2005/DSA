df0742399e74ef0ce134d14741c002b92df64506
Akash S <akashsenthilkumar2005@gmail.com>
2026-04-24 19:22:20 +0530

    Time: 1 ms (29.39%) | Memory: 43.1 MB (91.51%) - LeetSync

--- 4264-first-matching-character-from-both-ends/first-matching-character-from-both-ends.java
+++ 4264-first-matching-character-from-both-ends/first-matching-character-from-both-ends.java
@@ -3,10 +3,9 @@ class Solution {
         if(s.length()==1) return 0;
         int n=s.length();
         for(int i=0;i<s.length();i++){
-            int t=n-i-1;
             if(!(n-i-1<=0))
             {
-                    if(s.charAt(i)==s.charAt(t)) return i;
+                    if(s.charAt(i)==s.charAt(n-i-1)) return i;
             }
             else break;
         }
