60823bc45d8aa090194095475aa40c59bb51ea2a
Akash S <akashsenthilkumar2005@gmail.com>
2026-07-06 08:39:01 +0530

    Time: 1 ms (99.17%) | Memory: 42.4 MB (84.96%) - LeetSync

--- 4321-digit-frequency-score/digit-frequency-score.java
+++ 4321-digit-frequency-score/digit-frequency-score.java
@@ -1,12 +1,15 @@
 class Solution {
     public int digitFrequencyScore(int n) {
-       int s=0;
+        int[] a=new int[10];
         while(n!=0){
-            s+=n%10;
+            int t=n%10;
+            a[t]++;
             n/=10;
         }
-       
-        
+        int s=0;
+        for(int i=0;i<10;i++){
+            s+=(i*a[i]);
+        }
         return s;
     }
 }
